8f19b10f0040d57c1a84a2a5f36fdfb3ce572c40
Alan Drieux <alan.drieux@outlook.fr>
2019-01-16 16:58:06 +0100

    Sennd All modification

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var User     = require('../models/user');
 var passport = require('passport');
+var path    = require("path");
+var fs = require('fs');
 
 var isAuthenticated = function (req, res, next) {
     if (req.isAuthenticated())
@@ -12,11 +14,30 @@ var isAuthenticated = function (req, res, next) {
 
 /* GET users listing. */
 router.get('/', isAuthenticated,function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('dashboard', { title: 'Express' , path: req.path, user: req.user});
 });
+router.get('/path/:page', function (req, res) {
+    res.headersSent;
+    console.log(req.app.get('views'));
+    console.log("res.headersSent : ",res.headersSent);
+    fs.readFile('./views/'+req.params.page+'.ejs', function (err, html) {
+        if (err) {
+            throw err;
+        }
+        let page = html.toString('utf8')
+        console.log(Buffer.isBuffer(html));
+        res.set('Content-Type', 'text/html');
+        //res.send(page);
+        res.status(200).send(page);
+    });
 
-router.get('/dashboard', isAuthenticated,function(req, res, next) {
-    res.render('dashboard', { title: 'Express'  });
+    console.log("res.headersSent : ",res.headersSent);
+})
+router.get('/portfolios', isAuthenticated,function(req, res, next) {
+    res.render('portfolios', { title: 'Express' , path: req.path, user: req.user });
+});
+router.get('/CvManager', isAuthenticated,function(req, res, next) {
+    res.render('cv', { title: 'Express' , path: req.path, user: req.user });
 });
 
 // ========================================================
@@ -29,7 +50,6 @@ router.get('/login', function(req, res, next) {
     }
     res.render('login', { message: req.flash('error') });
     console.log(req.user);
-    console.log(req);
     console.log("Error Message : ",req.flash('error'))
 });
 router.post('/login', passport.authenticate('local-login', {
@@ -38,7 +58,12 @@ router.post('/login', passport.authenticate('local-login', {
         failureFlash: true
     }));
 
-router.get('/account', function (req, res) {
+router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+
+/*router.get('/account', function (req, res) {
     var newUser            = new User();
 
     newUser.email    = "alan.drieux@outlook.fr";
@@ -50,7 +75,7 @@ router.get('/account', function (req, res) {
 
         return res.json(newUser);
     });
-})
+})*/
 
 
 
